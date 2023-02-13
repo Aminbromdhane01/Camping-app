3826ef9b29c9eb05af96f515e49f85378517dcc0
AminBromdhane01 <mohamedaminebenromdhane.com>
2023-02-13 22:37:55 +0100

    craeting components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AuthorComponent } from './author/author.component';
+import { CreatearticleComponent } from './createarticle/createarticle.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path : '' , redirectTo : '/home' , pathMatch : 'full'},
+  {path : 'home' , component: HomeComponent},
+  {path : 'article/:id' , component : DetailComponent},
+  {path : 'create' , component : CreatearticleComponent},
+  {path : 'about' , component : AboutComponent},
+  {path : 'privacy' , component : PrivacyComponent},
+  {path : 'author/:id' , component : AuthorComponent},
+  {path : 'register' , component : RegisterComponent},
+  {path : 'login' , component : LoginComponent},
+  {path : '**' , component :NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
